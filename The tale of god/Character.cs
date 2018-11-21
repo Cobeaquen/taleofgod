5006dd98912b48f22e25c1d2867b7a335202b0dd
Cobeaquen <37908218+Cobeaquen@users.noreply.github.com>
2018-11-21 22:22:52 +0100

    update

---
+++ The tale of god/Character.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TheTaleOfGod
+{
+    public class Character
+    {
+        public float speed = 5f;
+
+        public Vector2 position;
+        public Texture2D sprite; // customize this with clothes
+        public Vector2 origin;
+
+        private Vector2 move;
+
+        public Character()
+        {
+            position = new Vector2(100, 100);
+        }
+
+        public void LoadCharacter()
+        {
+            sprite = Game1.content.Load<Texture2D>("textures\\chr1\\chr_head");
+            origin = new Vector2(sprite.Width / 2f, sprite.Height / 2f);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            KeyboardState key = Keyboard.GetState();
+
+            move = Vector2.Zero;
+
+            if (key.IsKeyDown(Keys.D))
+            {
+                move.X += speed;
+            }
+            else if (key.IsKeyDown(Keys.A))
+            {
+                move.X -= speed;
+            }
+            if (key.IsKeyDown(Keys.W))
+            {
+                move.Y -= speed;
+            }
+            else if (key.IsKeyDown(Keys.S))
+            {
+                move.Y += speed;
+            }
+
+            position += move;
+        }
+
+        public void Draw(SpriteBatch batch)
+        {
+            batch.Draw(sprite, position, null, Color.White, 0f, origin, 4f, SpriteEffects.None, 0f);
+        }
+    }
+}
---
+++ The tale of god/Game1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace TheTaleOfGod
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Character character = new Character();
+
+        public static ContentManager content { get; set; }
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            content = Content;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            character.LoadCharacter();
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            character.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null, null, null);
+
+            character.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
